d61c1ddb617b1bae50144e0cdc459afe4d945598
Zachariah Derrick <zachariahderrick@gmail.com>
2021-09-27 20:12:13 -0700

    box

---
+++ src/components/OswaldTheme.js
@@ -0,0 +1,9 @@
+import { createTheme } from '@material-ui/core/styles';
+
+export default createTheme({
+  typography: {
+    fontFamily: [
+      'Oswald',
+      'sans-serif',
+    ].join(','),
+  },});
--- src/components/VSCodeBox.js
+++ src/components/VSCodeBox.js
@@ -17,6 +17,9 @@ function VSCodeBox() {
 
                 <div class="maintext">
                     <text>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>1</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
                         <tspan>const</tspan>
                         <tspan>&nbsp;</tspan>
                         <tspan>express</tspan>
@@ -30,8 +33,190 @@ function VSCodeBox() {
                         <tspan>;</tspan>
                     </text><br/>
                     <text>
-                        1234console.log(`Currently listening on port: $1portNum}. Access it here: http://localhost:$1portNum}`);
-                    </text>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>2</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                        <tspan>let</tspan>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>app</tspan>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>=</tspan>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>express</tspan>
+                        <tspan>()</tspan>
+                        <tspan>;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>3</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                        <tspan>const</tspan>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>port</tspan>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>=</tspan>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>8080</tspan>
+                        <tspan>;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>4</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>5</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>6</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>7</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>8</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>&nbsp;</tspan>
+                        <tspan>9</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>10</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>11</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>12</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>13</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>14</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>15</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>16</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>17</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>18</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>19</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>20</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>21</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>22</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>23</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>24</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>25</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>26</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>27</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>28</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>29</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>30</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>31</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>32</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>33</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>34</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>35</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>36</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>37</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>38</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>39</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>40</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
+                    <text>
+                        <tspan>41</tspan>
+                        <tspan>&nbsp;&nbsp;&nbsp;&nbsp;</tspan>
+                    </text><br />
                 </div>
             </div>
 
